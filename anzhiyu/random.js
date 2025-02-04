e3493196c4c2d99b7e1741b1a2286ffa36237932
leotrump <leotrump48@gmail.com>
2025-02-04 16:47:37 +0800

    Site updated: 2025-02-04 16:47:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/29/hello-world/"];function toRandomPost(){
+var posts=["2025/01/29/hello-world/","2025/02/05/Henu信安期末回忆/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
